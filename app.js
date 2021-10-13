57e81e3eed4894f794fd01d294352a4c8e3e0e69
Isaac Oduh <ufuomaoduh@gmail.com>
2021-10-13 16:00:20 +0100

    setup db

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 const express = require('express');
+const colors = require('colors');
+const morgan = require('morgan');
 require('dotenv').config();
 
 const app = express();
+
+if (process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'))
+}
+  
 app.use(express.json());
 module.exports = app;
